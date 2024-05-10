016ff177263e44e27e4b360109f2549d5653e34e
wzp <2859572179@qq.com>
2024-05-11 00:09:18 +0800

    缓存套餐 v1.0

--- sky-server/src/main/java/com/sky/controller/user/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/user/CategoryController.java
@@ -6,6 +6,8 @@ import com.sky.service.admin.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
